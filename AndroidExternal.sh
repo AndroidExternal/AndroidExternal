ffb2808f24b89cdb7e484e7248464fc43dbe4ea2
Joe Maples <frap129@gmail.com>
2016-06-27 10:28:16 -0400

    Annotate script

--- AndroidExternal.sh
+++ AndroidExternal.sh
@@ -4,29 +4,28 @@
 # gives unlimited permission to copy and/or distribute it,
 # with or without modifications, as long as this notice is preserved.
 
-unset RMONLY
-
-while getopts ":a" opt; do
+# Check for options
+while getopts "r" opt; do
   case $opt in
     r)
       echo "Only removing directories." >&2
-      RMONLY=true;
+      for repos in "$(cat external/AndroidExternal.repos)"; do rm -rf external/$repos; done;
+      exit 0;
       ;;
     \?)
       echo "Usage: bash AndroidExternal.sh [-r]" >&2
+      exit 1;
       ;;
   esac
 done
 
+# Enter external directory
 cd external;
 
+# Update Android/External provided repos
 if [ -e "$(pwd)/AndroidExternal.repos" ]; then
   rm -rf AndroidExternal.repos;
-  if [ -n "$RMONLY" ]; then
-    exit 0;
-  fi;
 fi;
-
 if [ -n "$(which wget)" ]; then
   wget https://raw.githubusercontent.com/AndroidExternal/AndroidExternal/master/AndroidExternal.repos;
 elif [ -n "$(which curl)" ]; then
@@ -36,9 +35,11 @@ else
   exit 1;
 fi;
 
+# Sync repos
 for repos in "$(cat AndroidExternal.repos)"; do
   rm -rf $repos;
   git clone https://github.com/AndroidExternal/$repos -b mm;
 done;
 
+# Return to root.
 cd ..
